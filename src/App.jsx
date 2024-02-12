32df216edf8eedb15644ca1b26bc52e8296de103
muhammadsami <muhammadsami1242@gmail.com>
2024-02-12 14:23:11 +0500

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 function App() {
-  return;
+  return (
+    <main>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <h1>Navbar</h1>
+        <p>sdsdsdsd</p>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
