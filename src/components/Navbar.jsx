cbef069fbf3d403d3c251d48d720591a30fbdf61
muhammadsami <muhammadsami1242@gmail.com>
2024-02-13 01:06:37 +0500

    Adjust Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-      <div>
-        <h1>Navbar</h1>
-        <p>sdsdsdsd</p>
-      </div>
+      <header className="header">
+        <Link
+          to="/"
+          className="bg-white h-10 w-10 flex justify-center items-center rounded-lg shadow-md font-bold px-7 "
+        >
+          <p className="blue-gradient_text">Sami</p>
+        </Link>
+        <nav className="flex gap-7 text-lg font-medium">
+          <Link to="/about" className="hover:text-blue-500">
+            About
+          </Link>
+          <Link to="/projects" className="hover:text-blue-500">
+            Projects
+          </Link>
+        </nav>
+      </header>
     </>
   );
 };
