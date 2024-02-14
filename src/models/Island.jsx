ca1c022545abb248ff18a485d31da62f07d555fa
muhammadsami <muhammadsami1242@gmail.com>
2024-02-15 03:57:58 +0500

    fixed island component

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -8,7 +8,7 @@ Title: Fox's islands
 
 import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
-// import { useFrame, useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { a } from "@react-spring/three";
 
 import islandScene from "../assets/3d/island.glb";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,49 +3,49 @@ import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
 
-{
-  /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
-          popup
-        </div> */
-}
-
-const Home = () => {
+const Scene = () => {
   const adjustIslandForSceneSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
+
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
       screenScale = [1, 1, 1];
     }
-    return [screenPosition, screenScale, rotation];
+
+    return [screenScale, screenPosition, rotation];
   };
 
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForSceneSize();
 
   return (
-    <section className="w-full h-screen relative">
-      <Canvas
-        className="w-full h-screen bg-transparent"
-        camera={{ near: 0.1, far: 1000 }}
-      ></Canvas>
-      <Suspense fallback={<Loader />}>
-        {/* <directionalLight position={[1, 1, 1]} intensity={2} />
-        <ambientLight instensity={0.5} />
-        <hemisphereLight
-          skyColor="#b1e1ff"
-          groundColor="#000000"
-          intensity={1}
-        /> */}
+    <>
+      <directionalLight position={[1, 1, 1]} intensity={2} />
+      <ambientLight intensity={0.5} />
+      <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-        <Island
-          position={islandPosition}
-          scale={islandScale}
-          rotation={islandRotation}
-        />
-      </Suspense>
+      <Island
+        position={islandPosition}
+        scale={islandScale}
+        rotation={islandRotation}
+      />
+    </>
+  );
+};
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+      <div className="w-full h-screen bg-transparent">
+        <Canvas camera={{ near: 0.1, far: 1000 }}>
+          <Suspense fallback={<Loader />}>
+            <Scene />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
   );
 };
