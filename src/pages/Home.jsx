8e8691811460626d6d819245788327a8963cfa52
muhammadsami <muhammadsami1242@gmail.com>
2024-02-15 04:20:33 +0500

    added sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
-
+import Sky from "../models/Sky";
 const Scene = () => {
   const adjustIslandForSceneSize = () => {
     let screenScale = null;
@@ -26,7 +26,7 @@ const Scene = () => {
       <directionalLight position={[1, 1, 1]} intensity={2} />
       <ambientLight intensity={0.5} />
       <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
-
+      <Sky />
       <Island
         position={islandPosition}
         scale={islandScale}
